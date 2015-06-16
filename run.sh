3339303a25d50b40506825f030e83b25fb634f10
Benjamin Boutier <benjamin.boutier@gmail.com>
2015-06-16 23:59:58 +0200

    Fix indentation

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash	
+#!/bin/bash  
 INIT_DB_SCRIPT=/var/www/felinpossible/install/sql/light_dump.sql
 
 chown -R mysql:mysql /var/lib/mysql 
@@ -11,22 +11,22 @@ fi
 
 if [[ ! -d /var/lib/mysql/felinpossible ]]
 then
-    set -e
+  set -e
 
-    if [ -f ${INIT_DB_SCRIPT} ]
-    then
-      mysqld_safe &
-      sleep 10s
+  if [ -f ${INIT_DB_SCRIPT} ]
+  then
+    mysqld_safe &
+    sleep 10s
 
-      echo "Create felinpossible database..."
-	  mysql --user=root < ${INIT_DB_SCRIPT}
-	  echo "DB felinpossible created"
+    echo "Create felinpossible database..."
+    mysql --user=root < ${INIT_DB_SCRIPT}
+    echo "DB felinpossible created"
 
-	  killall mysqld	  
-	else
-	  echo "Init dump file ${INIT_DB_SCRIPT} not found"
-	  exit 1
-	fi
+    killall mysqld
+  else
+    echo "Init dump file ${INIT_DB_SCRIPT} not found"
+    exit 1
+  fi
 fi
 
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
